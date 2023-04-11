0eecc1acf497982efb8228b05ba69b1efb7c07ec
Santiago Vargas <ds.vargasp1@uniandes.edu.co>
2023-04-10 21:40:58 -0500

    Pequeños cambios

--- Docs/RFC4.sql
+++ Docs/RFC4.sql
@@ -6,5 +6,5 @@ LEFT OUTER JOIN A_Dispone ON A_Alojamiento.idAlojamiento = A_Dispone.idAlojamien
 LEFT OUTER JOIN A_Servicio ON A_Servicio.idServicio = A_Dispone.idServicio
 LEFT OUTER JOIN A_Reserva ON A_Reserva.idAlojamiento = A_Alojamiento.idAlojamiento
     WHERE (((A_Reserva.fechaInicio NOT BETWEEN ? AND ?) AND (A_Reserva.fechaFinal NOT BETWEEN ? AND ?)) OR
-           ((A_Reserva.fechaInicio BETWEEN ? AND ?) AND (A_Reserva.fechaFinal BETWEEN ? AND ?) AND 
+           ((A_Reserva.fechaInicio BETWEEN ? AND ?) OR (A_Reserva.fechaFinal BETWEEN ? AND ?) AND 
            A_Reserva.estado = 'Cancelada')) AND A_Servicio.nombre = ?;
